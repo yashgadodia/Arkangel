dfb89a16b3f3b6484be6b7e01ff08013f284ba00
nixxholas <nixholas@icloud.com>
2021-03-04 13:14:35 +0800

    plugins: fireark: uuid support and implementation
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- plugins/fireark.js
+++ plugins/fireark.js
@@ -1,16 +1,21 @@
 import Vue from 'vue';
 
 Vue.mixin({
-  methods:{
-    rpc (target, method, payload) {
+  methods: {
+    uuidv4() {
+      return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+      );
+    },
+    rpc(target, method, payload) {
       let res = {};
-      this.$fire.database.ref('rpc').set({
+      this.$fire.database.ref('rpc/' + this.uuidv4()).set({
         target: target,
         method: method,
         payload: payload
       })
         .catch(e => {
-          res = { success: false, msg: e};
+          res = {success: false, msg: e};
         })
         .then(r => {
           res = {
@@ -28,18 +33,18 @@ export default (context, inject) => {
   const rpc = (target, method, payload) => {
     let res = {};
     this.$fire.database.ref('rpc').set({
-        target: target,
-        method: method,
-        payload: payload
+      target: target,
+      method: method,
+      payload: payload
+    })
+      .catch(e => {
+        res = {success: false, msg: e};
       })
-        .catch(e => {
-          res = { success: false, msg: e};
-        })
-        .then(r => {
-          res = {
-            success: true, msg: "RPC Successfully sent"
-          };
-        });
+      .then(r => {
+        res = {
+          success: true, msg: "RPC Successfully sent"
+        };
+      });
 
     return res;
   };

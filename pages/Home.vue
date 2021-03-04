42c07ed8cf4fbfcb5239330399e25947b94c2f3b
Cindy <cif-zheng.xinxin@ninjavan.co>
2021-03-04 12:48:31 +0800

    pages

---
+++ pages/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <Navbar />
+    </div>
+</template>
+
+<script>
+import Navbar from "../components/Navbar.vue"
+
+export default {
+    name: "Home",
+    components: {
+        Navbar
+    }
+}
+</script>

fc54f6b84fe5884f7668d56557a4b26eea2ef7cb
moocowdrinksmilk <tayrx9988@gmail.com>
2021-03-21 12:11:06 +0800

    added user stuff

---
+++ components/UserItem.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="grid grid-cols-6 gap-2 mx-12">
+        <div>
+            {{name}}
+        </div>
+        <div>
+            {{phoneNumber}}
+        </div>
+        <div>
+            {{EName}}
+        </div>
+        <div>
+            {{EPhoneNumber}}
+        </div>
+        <div>
+            {{ERelationship}}
+        </div>
+        <div>
+            {{Photo}}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        name: String,
+        phoneNumber: Number,
+        EName: String,
+        EPhoneNumber: Number,
+        ERelationship: String,
+        Photo: String
+    }
+}
+</script>
--- pages/Users.vue
+++ pages/Users.vue
@@ -1,6 +1,36 @@
 <template>
     <div>
-      {{dataset}}
+      <Navbar/>
+      {{dataset[0]}}
+      <div class="grid grid-cols-6 gap-2 mx-12">
+        <div>
+          Name
+        </div>
+        <div>
+          Phone Number
+        </div>
+        <div>
+          Emergency Contact: <br> Name
+        </div>
+        <div>
+          Emergency Contact: <br> Phone Number
+        </div>
+        <div>
+          Emergency Contact: <br> Relationship
+        </div>
+        <div>
+          Photo
+        </div>
+      </div>
+      <div v-for="(item, index) in dataset" :key="index">
+          <user-item
+          :name = "item.name"
+          :phoneNumber = "item.phoneNo"
+          :EName = 'item.emergencyContacts'
+          >
+          Hello
+          </user-item>
+      </div>
     </div>
 </template>
 
@@ -9,16 +39,28 @@ export default {
     name: "Users",
   data: function() {
       return {
-        dataset: []
+        dataset: [],
+        eContacts: []
       }
   },
-  mounted() {
-      let curUsers = this.dataset;
-      this.databaseQuery('users').then(response => {
-        response.forEach(res => {
-          curUsers.push(res)
-        })
-      })
-  }
+  beforeUpdate(){
+    let x = this.dataset[0]
+    console.log(this.dataset);
+    this.dataset.forEach(element => {
+      console.log(element.emergencyContacts);
+    });
+  },
+  async fetch(){
+    this.dataset = await this.databaseQuery('users')
+  },
+  // mounted() {
+  //     let curUsers = this.dataset;
+  //     this.databaseQuery('users').then(response => {
+  //       response.forEach(res => {
+  //         curUsers.push(res)
+  //       })
+  //       console.log(this.dataset);
+  //     })
+  // }
 }
 </script>
